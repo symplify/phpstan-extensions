76f65ab3c7c05c4f932174af8e630bd0bbff294e
GitHub Action <action@github.com>
2024-12-12 10:30:28 +0000

    release PHP 7.4 downgraded

--- src/TypeExtension/MethodCall/LaravelContainerMakeTypeExtension.php
+++ src/TypeExtension/MethodCall/LaravelContainerMakeTypeExtension.php
@@ -17,9 +17,14 @@ use Symplify\PHPStanExtensions\TypeResolver\ClassConstFetchReturnTypeResolver;
  */
 final class LaravelContainerMakeTypeExtension implements DynamicMethodReturnTypeExtension
 {
-    public function __construct(
-        private readonly ClassConstFetchReturnTypeResolver $classConstFetchReturnTypeResolver
-    ) {
+    /**
+     * @readonly
+     */
+    private ClassConstFetchReturnTypeResolver $classConstFetchReturnTypeResolver;
+
+    public function __construct(ClassConstFetchReturnTypeResolver $classConstFetchReturnTypeResolver)
+    {
+        $this->classConstFetchReturnTypeResolver = $classConstFetchReturnTypeResolver;
     }
 
     public function getClass(): string
