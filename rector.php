1af4897b0cf6fba0d9097d62ea6994f3e2e567f9
Tomas Votruba <tomas.vot@gmail.com>
2025-01-07 09:30:44 +0000

    bump other configs

--- rector.php
+++ rector.php
@@ -3,33 +3,12 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([
-        PHPUnitSetList::PHPUNIT_100,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        LevelSetList::UP_TO_PHP_81,
-        SetList::CODING_STYLE,
-        SetList::TYPE_DECLARATION,
-        SetList::NAMING,
-        SetList::PRIVATIZATION,
-        SetList::EARLY_RETURN,
-        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-    ]);
-
-    $rectorConfig->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->importNames();
-
-    $rectorConfig->skip([
+return RectorConfig::configure()
+    ->withPaths([__DIR__ . '/src', __DIR__ . '/tests'])
+    ->withPreparedSets(codeQuality: true, deadCode: true, naming: true, privatization: true, earlyReturn: true, codingStyle: true)
+    ->withRootFiles()
+    ->withSkip([
         '*/Source/*',
         '*/Fixture/*',
     ]);
-};
